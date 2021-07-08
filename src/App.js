c14b36a8f6ebdbc698496ab612dee1148d00e781
qianchen <qianchen@douyu.tv>
2021-07-08 17:49:10 +0800

    feat: 添加title-bar

--- src/App.js
+++ src/App.js
@@ -1,85 +1,86 @@
 import React from 'react'
 //import logo from './logo.svg';
-import AppLogin from './Login/login'
-// import './App.css';
-import './AppTest11.css'
+// import AppLogin from './Login/login'
+import './App1.css';
+//import './AppTest11.css'
 
 class EasyShopping extends React.Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-          
-      //   </header>
-      //   <AppLogin/>
-      // </div>
       <body>
         <div id="__APP">
           <div class="css-1i3ha4y">
             <div class="css-13eeyma">
-              <a href="https://www.binancezh.sh/zh-CN">
-                <img src="https://static-file-1259603563.file.myqcloud.com/static/images/common/nav-logo.svg" alt="Binance"></img>
-              </a>
-              <div class="css-x84wzl">
-                <div class="css-ybbx55">
-                  <div class="css-11yl1bg">
-                    <div data-bn-type="text" class="css-1gybehy">简体中文</div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" class="css-1iztezc">
-                      <path d="M16 9v1.2L12 15l-4-4.8V9h8z" fill="currentColor"></path>
-                    </svg>
-                  </div>
-                  {/* <div class="css-1gai8bq" data-popper-reference-hidden="true" data-popper-escaped="true" data-popper-placement="bottom-end"> */}
-                  <div class="css-gv1gi9" data-popper-reference-hidden="false" data-popper-escaped="false" data-popper-placement="bottom-start">  
-                    <div class="css-168o2sb">
-                      <a class="" href="http://www.baidu.com">English</a>
-                      <a class="active" href="http://www.baidu.com">简体中文</a>
-                      <a class="" href="http://www.baidu.com">繁體中文 (台灣)</a>
-                      <a class="" href="http://www.baidu.com">繁體中文 (香港)</a>
-                      <a class="" href="http://www.baidu.com" >한국어</a>
-                      <a class="" href="http://www.baidu.com">Русский</a>
-                      <a class="" href="http://www.baidu.com">Español (Internacional)</a>
-                      <a class="" href="http://www.baidu.com">Español (Latinoamérica)</a>
-                      <a class="" href="http://www.baidu.com">Español (México)</a>
-                      <a class="" href="http://www.baidu.com">Français</a>
-                      <a class="" href="http://www.baidu.com">Deutsch</a>
-                      <a class="" href="http://www.baidu.com">Tiếng Việt</a>
-                      <a class="" href="http://www.baidu.com">Türkçe</a>
-                      <a class="" href="http://www.baidu.com">Vlaams</a>
-                      <a class="" href="http://www.baidu.com">Português</a
-                      ><a class="" href="http://www.baidu.com">Italiano</a>
-                      <a class="" href="http://www.baidu.com">Polski</a>
-                      <a class="" href="http://www.baidu.com">Bahasa Indonesia</a>
-                      <a class="" href="http://www.baidu.com">Українська</a>
-                      <a class="" href="http://www.baidu.com">Filipino</a>
-                      <a class="" href="http://www.baidu.com">日本</a>
-                      <a class="" href="http://www.baidu.com">English (Australia)</a>
-                      <a class="" href="http://www.baidu.com">Português (Brasil)</a>
-                      <a class="" href="http://www.baidu.com">ไทย</a>
-                      <a class="" href="http://www.baidu.com">English (India)</a>
-                      <a class="" href="http://www.baidu.com">English (Nigeria)</a>
-                      <a class="" href="http://www.baidu.com">Română</a>
-                      <a class="" href="http://www.baidu.com">български</a>
-                      <a class="" href="http://www.baidu.com">Čeština</a>
-                      <a class="" href="http://www.baidu.com">latviešu valoda</a>
-                      <a class="" href="http://www.baidu.com">বাংলা</a>
-                      <a class="" href="http://www.baidu.com">svenska</a>
-                      <a class="" href="http://www.baidu.com">Português (Portugal)</a>
-                      <a class="" href="http://www.baidu.com">العربية</a>
-                      <a class="" href="http://www.baidu.com">Suomi</a>
-                      <a class="" href="http://www.baidu.com">Norsk</a>
-                      <a class="" href="http://www.baidu.com">हिंदी</a>
-                    </div>
-                  </div>
+              <div class="titleBar-logo-container">
+                <div class="titleBar-logo">
+                  <a href="https://www.binancezh.sh/zh-CN">
+                    <img src="https://static-file-1259603563.file.myqcloud.com/static/images/common/nav-logo.svg" alt="Binance"></img>
+                  </a>
+                </div>
+              </div>
+              <div class="titleBar-container2">
+                <div class="titleBar-language-container">
+                  <div data-bn-type="text" class="language-switch">简体中文</div>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" class="language-switch-svg">
+                    <path d="M16 9v1.2L12 15l-4-4.8V9h8z" fill="currentColor"></path>
+                  </svg>
                 </div>
               </div>
               <main class="main css-1xy7yb4">
                 <div class="css-moj0yw">
                   <div class="css-yqufh">
-                      <AppLogin class="appLogin-css"/>
+                      
                   </div>
                 </div>
               </main>
             </div>
+            <div class="titleBar-language-expand-container">
+              {/* <div class="css-1gai8bq" data-popper-reference-hidden="true" data-popper-escaped="true" data-popper-placement="bottom-end"> */}
+              <div class="titleBar-language-expand-container-child" data-popper-reference-hidden="false" data-popper-escaped="false" data-popper-placement="bottom-start">  
+                <div class="titleBar-language-expand-container-child1">
+                  <a class="" href="http://www.baidu.com">English</a>
+                  <a class="active" href="http://www.baidu.com">简体中文</a>
+                  <a class="" href="http://www.baidu.com">繁體中文 (台灣)</a>
+                  <a class="" href="http://www.baidu.com">繁體中文 (香港)</a>
+                  <a class="" href="http://www.baidu.com" >한국어</a>
+                  <a class="" href="http://www.baidu.com">Русский</a>
+                  <a class="" href="http://www.baidu.com">Español (Internacional)</a>
+                  <a class="" href="http://www.baidu.com">Español (Latinoamérica)</a>
+                  <a class="" href="http://www.baidu.com">Español (México)</a>
+                  <a class="" href="http://www.baidu.com">Français</a>
+                  <a class="" href="http://www.baidu.com">Deutsch</a>
+                  <a class="" href="http://www.baidu.com">Tiếng Việt</a>
+                  <a class="" href="http://www.baidu.com">Türkçe</a>
+                  <a class="" href="http://www.baidu.com">Vlaams</a>
+                  <a class="" href="http://www.baidu.com">Português</a>
+                  <a class="" href="http://www.baidu.com">Italiano</a>
+                  <a class="" href="http://www.baidu.com">Polski</a>
+                  <a class="" href="http://www.baidu.com">Bahasa Indonesia</a>
+                  <a class="" href="http://www.baidu.com">Українська</a>
+                  <a class="" href="http://www.baidu.com">Filipino</a>
+                  <a class="" href="http://www.baidu.com">日本</a>
+                  <a class="" href="http://www.baidu.com">English (Australia)</a>
+                  <a class="" href="http://www.baidu.com">Português (Brasil)</a>
+                  <a class="" href="http://www.baidu.com">ไทย</a>
+                  <a class="" href="http://www.baidu.com">English (India)</a>
+                  <a class="" href="http://www.baidu.com">English (Nigeria)</a>
+                  <a class="" href="http://www.baidu.com">Română</a>
+                  <a class="" href="http://www.baidu.com">български</a>
+                  <a class="" href="http://www.baidu.com">Čeština</a>
+                  <a class="" href="http://www.baidu.com">latviešu valoda</a>
+                  <a class="" href="http://www.baidu.com">বাংলা</a>
+                  <a class="" href="http://www.baidu.com">svenska</a>
+                  <a class="" href="http://www.baidu.com">Português (Portugal)</a>
+                  <a class="" href="http://www.baidu.com">العربية</a>
+                  <a class="" href="http://www.baidu.com">Suomi</a>
+                  <a class="" href="http://www.baidu.com">Norsk</a>
+                  <a class="" href="http://www.baidu.com">हिंदी</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            {/* <AppLogin class="appLogin-css"/> */}
           </div>
         </div> 
       </body>
